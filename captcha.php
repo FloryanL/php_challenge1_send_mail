e2b5748d1ae292e51fa67989f57978c034b5ec1d
FloryanL <floryan.lollivier@gmail.com>
2016-08-29 17:28:53 +0200

    ajout fichier captcha

---
+++ captcha.php
@@ -0,0 +1,23 @@
+<?php
+    function motListe()
+    {
+        $liste = array_merge(range('a','z'),range('A','Z'),range('0','9'))/*array('internet', 'captcha', 'robot');*/;
+        return $liste[array_rand($liste)];
+    }
+
+    function captcha()
+    {
+        $mot = motListe();
+        $_SESSION['captcha'] = $mot;
+        return $mot;
+    }/*
+    function motHasard($n)
+    {
+        $lettres = array_merge(range('a','z'),range('A','Z'),range('0','9'));
+        $nl = count($lettres)-1;
+        $mot = '';
+        for($i = 0; $i < $n; ++$i)
+            $mot .= $lettres[mt_rand(0,$nl)];
+        return $mot;
+    }*/
+?>
